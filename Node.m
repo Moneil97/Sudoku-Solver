fff3f2a89d1d53f7cbb71651550ec46bf1400d15
Cameron ONeil <cam8059@yahoo.com>
2018-05-15 23:14:09 -0500

    Brute Force Added 2

--- Node.m
+++ Node.m
@@ -4,18 +4,62 @@ classdef Node < handle
         isSolved
         value
         possibleValues
+        currentIndex %used for brute force
     end
     
     methods
         %constructor
-        function obj = Node(val)
+        function this = Node(val)
             if nargin == 0
-                obj.isSolved = 0;
-                obj.value = 0;
+                this.isSolved = 0;
+                this.value = 0;
             else
-                obj.isSolved = 1;
-                obj.value = val;
+                this.isSolved = 1;
+                this.value = val;
+            end
+            this.currentIndex = 1;
+        end
+        
+        %increment index for brute force check
+        function inc(this, nodes, r, c)
+            
+            %if called on a solved node then just inc the next one instead
+            if this.isSolved
+                if c >= 9
+                    if r >= 9
+                        disp("r == 9");
+                        return;
+                    end
+                    nodes(r+1,1).inc(nodes, r+1, 1);
+                else
+                    nodes(r,c+1).inc(nodes, r, c+1);
+                end
+                
+                return;
+            end
+            
+            
+            this.currentIndex = this.currentIndex + 1;
+            if this.currentIndex > length(this.possibleValues)
+                this.currentIndex = 1;
+                if c >= 9
+                    %if r also == 9 then no solution
+                    %end
+                    if r >= 9
+                        disp("r === 9");
+                        return;
+                    end
+                    nodes(r+1,1).inc(nodes, r+1, 1);
+                else
+                    nodes(r,c+1).inc(nodes, r, c+1);
+                end
+                
             end
         end
+        
+        function val = getBFval(this)
+            val = this.possibleValues(this.currentIndex);
+        end
+        
     end
 end
